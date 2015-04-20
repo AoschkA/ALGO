f22cfdeeb361935177caa5cffd0489f5cb16fe1b
AoschkA <jonaspraem@gmail.com>
2015-04-20 10:50:39 +0200

    intro

--- ALGO_Projekt/src/assignments/SizeOfNetwork.java
+++ ALGO_Projekt/src/assignments/SizeOfNetwork.java
@@ -1,5 +1,29 @@
 package assignments;
 
-public class SizeOfNetwork {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class SizeOfNetwork {
+	int numberofpersons = 1;
+	
+	public void run() throws IOException {
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		String users = input.readLine();
+		
+		for (int i=0; i<users.length();i++) {
+		if (users.charAt(i)==' ') {
+			numberofpersons++;
+		}
+		}
+		
+		boolean[][] f = new boolean[numberofpersons][numberofpersons]; 
+		
+		
+		
+		
+	}
+	
 }
+
+
