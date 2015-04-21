c021e4ab74e31dbf838f576a4176bcfa1c9309e9
AoschkA <jonaspraem@gmail.com>
2015-04-21 11:54:33 +0200

    virker nu

--- ALGO_Projekt/src/assignments/SizeOfNetwork.java
+++ ALGO_Projekt/src/assignments/SizeOfNetwork.java
@@ -3,27 +3,55 @@ package assignments;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class SizeOfNetwork {
 	int numberofpersons = 1;
+	int numberoffriends = 0;
+	int freindTwo;
+	int freindOne;
+	boolean morefriends = true;
 	
 	public void run() throws IOException {
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 		String users = input.readLine();
-		
 		for (int i=0; i<users.length();i++) {
 		if (users.charAt(i)==' ') {
 			numberofpersons++;
 		}
 		}
 		
-		boolean[][] f = new boolean[numberofpersons][numberofpersons]; 
 		
+		while (morefriends) {
+		String temp = input.readLine();
+		if (temp.contains("stoerrelse")) {
+			morefriends = false;
+			break;
+		}
 		
+		StringTokenizer tokenizer = new StringTokenizer(temp, " ");
 		
+		try {
+		freindOne = Integer.parseInt(tokenizer.nextToken());
+		freindTwo = Integer.parseInt(tokenizer.nextToken());
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
 		
+		numberoffriends++;
+		}
+		
+		input.close();
+		System.out.println(numberofpersons+ " "+ numberoffriends);
+		
+	}
+	public static void main(String[] args) throws IOException {
+		SizeOfNetwork s = new SizeOfNetwork();
+		s.run();
 	}
 	
 }
+
 
 
